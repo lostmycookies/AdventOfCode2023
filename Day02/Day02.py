d347f8a4fc60dc6a780558fde89483eca3d8cf61
lostmycookies <polzin.benjamin@googlemail.com>
2025-06-15 14:37:39 +0200

    On branch master Changes to be committed: 	new file:   Day02/Day02.py 	new file:   Day02/input.txt

---
+++ Day02/Day02.py
@@ -0,0 +1,8 @@
+# Read in the input file
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+# Erase the leading characters from each line
+print(lines)
+
+# Check if game is possible
